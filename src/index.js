a8d702677dc06a9248a87e847fc902c8c6de6579
曹强 <cq872286926@163.com>
2024-05-31 14:33:33 +0800

    del:

--- src/index.js
+++ src/index.js
@@ -21,33 +21,6 @@ class App extends React.Component {
         return (<Provider store={store}>
             <div>
                 <Menu mode="horizontal" onClick={this.handleClick} selectedKeys={[current]}>
-                    <Menu.Item key="gameChess">
-                        <a href="#/gameChess">gameChess</a>
-                    </Menu.Item>
-                    <Menu.Item key="coreContent">
-                        <a href="#/coreContent">coreContent</a>
-                    </Menu.Item>
-                    <Menu.Item key="seniorGuidance">
-                        <a href="#/seniorGuidance">seniorGuidance</a>
-                    </Menu.Item>
-                    <Menu.Item key="api">
-                        <a href="#/api">Api</a>
-                    </Menu.Item>
-                    <Menu.Item key="hook">
-                        <a href="#/hook">Hook</a>
-                    </Menu.Item>
-                    <Menu.Item key="faq">
-                        <a href="#/faq">FAQ</a>
-                    </Menu.Item>
-                    <Menu.Item key="redux">
-                        <a href="#/redux">Redux</a>
-                    </Menu.Item>
-                    <Menu.Item key="about">
-                        <a href="#/about">About</a>
-                    </Menu.Item>
-                    <Menu.Item key="strategy">
-                        <a href="#/strategy">Strategy</a>
-                    </Menu.Item>
                     <Menu.Item key="sell">
                         <a href="#/sell">Sell</a>
                     </Menu.Item>
--- src/router/router.js
+++ src/router/router.js
@@ -3,14 +3,6 @@ import {HashRouter, Route, Switch, Redirect} from 'react-router-dom';
 // import GameChess from "../components/gameChess";
 // import CoreContent from "../components/coreContent";
 // import SeniorGuidance from "../components/seniorGuidance";
-const GameChess = lazy(() => import('../components/gameChess/index.tsx'));
-const CoreContent = lazy(() => import('../components/coreContent'));
-const SeniorGuidance = lazy(() => import('../components/seniorGuidance'));
-const Api = lazy(() => import('../components/api'));
-const Hook = lazy(() => import('../components/hook'));
-const Faq = lazy(() => import('../components/faq'));
-const Redux = lazy(() => import('../components/redux'));
-const Strategy = lazy(() => import('../components/strategy'));
 const Sell = lazy(() => import('../components/sell'));
 const SellF = lazy(() => import('../components/sellF'));
 const SellFilter = lazy(() => import('../components/SellFilter'));
@@ -19,26 +11,11 @@ const Instruction = lazy(() => import('../components/instruction'));
 const Performance = lazy(() => import('../components/performance'));
 const Rtquery = lazy(() => import('../components/rtquery'));
 
-class About extends React.Component{
-    render() {
-      return <h3>About</h3>
-    }
-}
-
 const BasicRoute = () => (
     <HashRouter>
         <Suspense fallback={<div>Loading...</div>}>
             <Switch>
                 <Route exact path="/" render={() => <Redirect to="/sell" push />} />
-                <Route exact path="/gameChess" component={GameChess}/>
-                <Route exact path="/coreContent" component={CoreContent}/>
-                <Route exact path="/seniorGuidance" component={SeniorGuidance}/>
-                <Route exact path="/api" component={Api}/>
-                <Route exact path="/hook" component={Hook}/>
-                <Route exact path="/faq" component={Faq}/>
-                <Route exact path="/redux" component={Redux}/>
-                <Route exact path="/about" component={About}/>
-                <Route exact path="/strategy" component={Strategy}/>
                 <Route exact path="/sell" component={Sell}/>
                 <Route exact path="/sellF" component={SellF}/>
                 <Route exact path="/sellFilter" component={SellFilter}/>
