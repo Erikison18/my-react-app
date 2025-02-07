f1d63b71d493f9a0013537b54503629157a5175b
曹强 <cq872286926@163.com>
2025-02-07 11:21:36 +0800

    add

--- src/components/AllMarket/index.js
+++ src/components/AllMarket/index.js
@@ -65,11 +65,11 @@ function App() {
             // ) {
             if (parseInt(item.max_withdraw) > 25) {
                 myReturn = false;
-            // } else if (parseInt(item.annual_return) > 80 && item.score > 75) {
-            //     ids.push(item.id);
-            //     myReturn = true;
-            } else if (((item.tag.includes('大盘') || item.tag.includes('沪深300') || item.tag.includes('中证500')) && parseInt(item.annual_return) > 40)) {
+            } else if (parseInt(item.annual_return) > 100 && item.score > 80) {
+                ids.push(item.id);
                 myReturn = true;
+            // } else if (((item.tag.includes('大盘') || item.tag.includes('沪深300') || item.tag.includes('中证500')) && parseInt(item.annual_return) > 40)) {
+            //     myReturn = true;
             // } else if (((item.tag.includes('小盘') || item.tag.includes('次新')) && item.cnt >= 3 && item.cnt <= 5 && parseInt(item.annual_return) > 100 && item.score > 75)) {
             //     myReturn = true;
             } else {
